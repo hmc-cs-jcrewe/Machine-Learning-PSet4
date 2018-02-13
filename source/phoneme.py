052d67326057abd40d6a7638c690851569cbd1be
mguills <mattjguil@gmail.com>
2018-02-12 23:24:41 -0800

    Started second part

--- source/phoneme.py
+++ source/phoneme.py
@@ -1,5 +1,5 @@
 """
-Author      : Jackson Crewe & Matt Guillory 
+Author      : Jackson Crewe & Matt Guillory
 Class       : HMC CS 158
 Date        : 2018 Feb 14
 Description : Perceptron vs Logistic Regression on a Phoneme Dataset
@@ -25,55 +25,55 @@ from sklearn.linear_model import Perceptron, LogisticRegression
 def cv_performance(clf, train_data, kfs) :
     """
     Determine classifier performance across multiple trials using cross-validation
-    
+
     Parameters
     --------------------
         clf        -- classifier
         train_data -- Data, training data
         kfs        -- array of size n_trials
                       each element is one fold from model_selection.KFold
-    
+
     Returns
     --------------------
         scores     -- numpy array of shape (n_trials, n_fold)
                       each element is the (accuracy) score of one fold in one trial
     """
-    
+
     n_trials = len(kfs)
     n_folds = kfs[0].n_splits
     scores = np.zeros((n_trials, n_folds))
-    
+
     ### ========== TODO : START ========== ###
     # part b: run multiple trials of CV
-    
+
     ### ========== TODO : END ========== ###
-    
+
     return scores
 
 
 def cv_performance_one_trial(clf, train_data, kf) :
     """
     Compute classifier performance across multiple folds using cross-validation
-    
+
     Parameters
     --------------------
         clf        -- classifier
         train_data -- Data, training data
         kf         -- model_selection.KFold
-    
+
     Returns
     --------------------
         scores     -- numpy array of shape (n_fold, )
                       each element is the (accuracy) score of one fold
     """
-    
+
     scores = np.zeros(kf.n_splits)
-    
+
     ### ========== TODO : START ========== ###
     # part b: run one trial of CV
-    
+
     ### ========== TODO : END ========== ###
-    
+
     return scores
 
 
@@ -83,25 +83,27 @@ def cv_performance_one_trial(clf, train_data, kf) :
 
 def main() :
     np.random.seed(1234)
-    
+
     #========================================
     # load data
     train_data = load_data("phoneme_train.csv")
-    
+
     ### ========== TODO : START ========== ###
-    # part a: is data linearly separable?
-    
+    clf = Perceptron()
+    clf.fit(train_data.X, train_data.Y)
+    print 'coef = %s' % (str(clf.coef_))
+
     ### ========== TODO : END ========== ###
-        
+
     ### ========== TODO : START ========== ###
     # part c-d: compare classifiers
     # make sure to use same folds across all runs
-    
+
     ### ========== TODO : END ========== ###
-    
+
     ### ========== TODO : START ========== ###
     # part e: plot
-    
+
     ### ========== TODO : END ========== ###
 
 if __name__ == "__main__" :
