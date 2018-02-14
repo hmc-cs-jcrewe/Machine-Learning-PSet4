8c6424869ec9b92431618c1bf448dc23cbfbf463
mguills <mattjguil@gmail.com>
2018-02-13 20:38:22 -0800

    finished parts b-d

--- source/perceptron.py
+++ source/perceptron.py
@@ -1,5 +1,5 @@
 """
-Author      : Jackson Crewe & Matt Guillory 
+Author      : Jackson Crewe & Matt Guillory
 Class       : HMC CS 158
 Date        : 2017 Feb 14
 Description : Perceptron
@@ -19,14 +19,14 @@ from sklearn.svm import SVC
 
 def load_simple_dataset(start=0, outlier=False) :
     """Simple dataset of three points."""
-    
+
     #  dataset
     #     i    x^{(i)}      y^{(i)}
     #     1    (-1, 1)^T    1
     #     2    (0, -1)^T    -1
     #     3    (1.5, 1)^T   1
     #   if outlier is set, x^{(3)} = (12, 1)^T
-    
+
     # data set
     data = Data()
     data.X = np.array([[ -1, 1],
@@ -35,27 +35,27 @@ def load_simple_dataset(start=0, outlier=False) :
     if outlier :
         data.X[2,:] = [12, 1]
     data.y = np.array([1, -1, 1])
-    
+
     # circularly shift the data points
     data.X = np.roll(data.X, -start, axis=0)
     data.y = np.roll(data.y, -start)
-    
+
     return data
 
 
 def plot_perceptron(data, clf, plot_data=True, axes_equal=False, **kwargs) :
     """Plot decision boundary and data."""
     assert isinstance(clf, Perceptron)
-    
+
     # plot options
     if "linewidths" not in kwargs :
         kwargs["linewidths"] = 2
     if "colors" not in kwargs :
         kwargs["colors"] = 'k'
-    
+
     # plot data
     if plot_data : data.plot()
-    
+
     # axes limits and properties
     xmin, xmax = data.X[:, 0].min() - 1, data.X[:, 0].max() + 1
     ymin, ymax = data.X[:, 1].min() - 1, data.X[:, 1].max() + 1
@@ -64,23 +64,23 @@ def plot_perceptron(data, clf, plot_data=True, axes_equal=False, **kwargs) :
         xmax = ymax = max(xmax, ymax)
         plt.xlim(xmin, xmax)
         plt.ylim(ymin, ymax)
-    
+
     # create a mesh to plot in
     h = .02  # step size in the mesh
     xx, yy = np.meshgrid(np.arange(xmin, xmax, h), np.arange(ymin, ymax, h))
-    
+
     # determine decision boundary
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
 
     # plot decision boundary
     Z = Z.reshape(xx.shape)
     CS = plt.contour(xx, yy, Z, [0], **kwargs)
-    
+
     # legend
     if "label" in kwargs :
         #plt.clabel(CS, inline=1, fontsize=10)
         CS.collections[0].set_label(kwargs["label"])
-    
+
     plt.show()
 
 
@@ -89,11 +89,11 @@ def plot_perceptron(data, clf, plot_data=True, axes_equal=False, **kwargs) :
 ######################################################################
 
 class Perceptron :
-    
+
     def __init__(self) :
         """
         Perceptron classifier that keeps track of mistakes made on each data point.
-        
+
         Attributes
         --------------------
             coef_     -- numpy array of shape (d,), feature weights
@@ -101,38 +101,38 @@ class Perceptron :
         """
         self.coef_ = None
         self.mistakes_ = None
-    
+
     def fit(self, X, y, coef_init=None, verbose=False) :
         """
         Fit the perceptron using the input data.
-        
+
         Parameters
         --------------------
             X         -- numpy array of shape (n,d), features
             y         -- numpy array of shape (n,), targets
             coef_init -- numpy array of shape (n,d), initial feature weights
             verbose   -- boolean, for debugging purposes
-        
+
         Returns
         --------------------
             self      -- an instance of self
         """
         # get dimensions of data
         n,d = X.shape
-        
+
         # initialize weight vector to all zeros
         if coef_init is None :
             self.coef_ = np.zeros(d)
         else :
             self.coef_ = coef_init
-        
+
         # record number of mistakes we make on each data point
         self.mistakes_ = np.zeros(n)
-        
+
         # debugging
         if verbose :
             print '\ttheta^{(0)} = %s' % str(self.coef_)
-        
+
         ### ========== TODO : START ========== ###
         # part a: implement perceptron algorithm
         # cycle until all examples are correctly classified
@@ -146,21 +146,21 @@ class Perceptron :
         # do NOT shuffle examples on each iteration
         # on a mistake, be sure to update self.mistakes_
         #               and if verbose, output the updated self.coef_
-        
+
         ### ========== TODO : END ========== ###
-        
+
         return self
-    
+
     def fitHelper(self, X, y):
         """
-        Determine if there aren't any mistakes in the classification of the training data 
-        
+        Determine if there aren't any mistakes in the classification of the training data
+
         Parameters
         --------------------
             X         -- numpy array of shape (n,d), features
             y         -- numpy array of shape (n,), targets
-            
-        
+
+
         Returns
         --------------------
             bool      -- true if there are no mistakes in the training data classification, false otherwise
@@ -177,11 +177,11 @@ class Perceptron :
     def predict(self, X) :
         """
         Predict labels using perceptron.
-        
+
         Parameters
         --------------------
             X         -- numpy array of shape (n,d), features
-        
+
         Returns
         --------------------
             y_pred    -- numpy array of shape (n,), predictions
@@ -194,10 +194,10 @@ class Perceptron :
 ######################################################################
 
 def main() :
-    
+
     #========================================
     # test simple data set
-    
+
     # starting with data point $x^{(1)}$ without outlier
     #   coef = [ 0.  1.], mistakes = 1
     # starting with data point $x^{(2)}$ without outlier
@@ -217,7 +217,7 @@ def main() :
             clf.fit(data.X, data.y, verbose=False)
             plt.title(text)
             print '\tcoef = %s, mistakes = %d' % (str(clf.coef_), sum(clf.mistakes_))
-    
+
     ### ========== TODO : START ========== ###
     train_data = load_data("perceptron_data.csv")
     clf2 = Perceptron()
@@ -228,25 +228,25 @@ def main() :
     clf3.fit(train_data.X, train_data.y, np.array([1,0]))
     print 'non-0 initial: ', '\tcoef = %s, mistakes = %d' % (str(clf3.coef_), sum(clf3.mistakes_))
 
-    
+
     ### ========== TODO : END ========== ###
-    
-    
-    
+
+
+
     #========================================
     # perceptron data set
-    
+
     train_data = load_data("perceptron_data.csv")
-    
+
     # you do not have to understand this code -- we will cover it when we discuss SVMs
     # compute gamma^2 using hard-margin SVM (SVM with large C)
     clf = SVC(kernel='linear', C=1e10)
     clf.fit(train_data.X, train_data.y)
     gamma = 1./np.linalg.norm(clf.coef_, 2)
-    
+
     ### ========== TODO : START ========== ###
     # part c: see handout
-    
+
     # compute R^2
     n,d = np.shape(train_data.X)
     R = 0
@@ -260,7 +260,7 @@ def main() :
 
     # compute perceptron bound (R / gamma)^2
     bound = (float(R) / float(gamma))**0.5
-    print 'bound: ', bound 
+    print 'bound: ', bound
     ### ========== TODO : EEND ========== ###
 
 if __name__ == "__main__" :
--- source/phoneme.py
+++ source/phoneme.py
@@ -44,7 +44,8 @@ def cv_performance(clf, train_data, kfs) :
     scores = np.zeros((n_trials, n_folds))
 
     ### ========== TODO : START ========== ###
-    # part b: run multiple trials of CV
+    for i in range(n_trials):
+        scores[i] = cv_performance_one_trial(clf, train_data, kfs[i])
 
     ### ========== TODO : END ========== ###
 
@@ -70,7 +71,13 @@ def cv_performance_one_trial(clf, train_data, kf) :
     scores = np.zeros(kf.n_splits)
 
     ### ========== TODO : START ========== ###
-    # part b: run one trial of CV
+    index = 0
+    for train_index, test_index in kf.split(train_data.X, train_data.y):
+        X_train, X_test = train_data.X[train_index], train_data.X[test_index]
+        y_train, y_test = train_data.y[train_index], train_data.y[test_index]
+        clf.fit(X_train, y_train)
+        scores[index] = clf.score(X_test, y_test)
+        index += 1
 
     ### ========== TODO : END ========== ###
 
@@ -89,15 +96,78 @@ def main() :
     train_data = load_data("phoneme_train.csv")
 
     ### ========== TODO : START ========== ###
-    clf = Perceptron()
-    clf.fit(train_data.X, train_data.Y)
-    print 'coef = %s' % (str(clf.coef_))
+    clf = Perceptron(max_iter = 1000)
+    clf.fit(train_data.X, train_data.y)
+    print 'score = %d' % clf.score(train_data.X, train_data.y)
 
     ### ========== TODO : END ========== ###
 
     ### ========== TODO : START ========== ###
-    # part c-d: compare classifiers
-    # make sure to use same folds across all runs
+
+    # create model_selection.KFold for computing classifier performance
+    kfs = [model_selection.KFold(n_splits=10, shuffle=True) for i in range(10)]
+
+    # computes classifier performance for each classifier
+    clf1 = Perceptron(max_iter = 1000)
+    perceptron_scores = cv_performance(clf1, train_data, kfs)
+    perceptron_scores = perceptron_scores.flatten()
+    print perceptron_scores
+
+    clf2 = DummyClassifier(strategy = 'most_frequent')
+    dummy_scores = cv_performance(clf2, train_data, kfs)
+    dummy_scores = dummy_scores.flatten()
+    print dummy_scores
+
+    clf3 = LogisticRegression(C = 1e5)
+    logistic_scores = cv_performance(clf3, train_data, kfs)
+    logistic_scores = logistic_scores.flatten()
+    print logistic_scores
+
+    perceptron_mean = np.mean(perceptron_scores, dtype=np.float64)
+    perceptron_sdev = np.std(perceptron_scores, dtype=np.float64)
+
+    dummy_mean = np.mean(dummy_scores, dtype=np.float64)
+    dummy_sdev = np.std(dummy_scores, dtype=np.float64)
+
+    logistic_mean = np.mean(logistic_scores, dtype=np.float64)
+    logistic_sdev = np.std(logistic_scores, dtype=np.float64)
+
+    print 'the perceptron mean is: %03.3f. The standard deviation is: %03.3f' % (perceptron_mean, perceptron_sdev)
+    print 'the dummy mean is: %03.3f. The standard deviation is: %03.3f' % (dummy_mean, dummy_sdev)
+    print 'the logistic mean is: %03.3f. The standard deviation is: %03.3f' % (logistic_mean, logistic_sdev)
+
+    X_scaled = preprocessing.scale(train_data.X)
+    train_scaled = Data(X_scaled, train_data.y)
+
+    # computes classifier performance for each classifier
+    clf1 = Perceptron(max_iter = 1000)
+    perceptron_scores_scaled = cv_performance(clf1, train_scaled, kfs)
+    perceptron_scores_scaled = perceptron_scores_scaled.flatten()
+    print perceptron_scores_scaled
+
+    clf2 = DummyClassifier(strategy = 'most_frequent')
+    dummy_scores_scaled = cv_performance(clf2, train_scaled, kfs)
+    dummy_scores_scaled = dummy_scores_scaled.flatten()
+    print dummy_scores_scaled
+
+    clf3 = LogisticRegression(C = 1e5)
+    logistic_scores_scaled = cv_performance(clf3, train_scaled, kfs)
+    logistic_scores_scaled = logistic_scores_scaled.flatten()
+    print logistic_scores_scaled
+
+    perceptron_mean_scaled = np.mean(perceptron_scores_scaled, dtype=np.float64)
+    perceptron_sdev_scaled = np.std(perceptron_scores_scaled, dtype=np.float64)
+
+    dummy_mean_scaled = np.mean(dummy_scores_scaled, dtype=np.float64)
+    dummy_sdev_scaled = np.std(dummy_scores_scaled, dtype=np.float64)
+
+    logistic_mean_scaled = np.mean(logistic_scores_scaled, dtype=np.float64)
+    logistic_sdev_scaled = np.std(logistic_scores_scaled, dtype=np.float64)
+
+    print 'the perceptron mean_scaled is: %03.3f. The standard deviation is: %03.3f' % (perceptron_mean_scaled, perceptron_sdev_scaled)
+    print 'the dummy mean_scaled is: %03.3f. The standard deviation is: %03.3f' % (dummy_mean_scaled, dummy_sdev_scaled)
+    print 'the logistic mean_scaled is: %03.3f. The standard deviation is: %03.3f' % (logistic_mean_scaled, logistic_sdev_scaled)
+
 
     ### ========== TODO : END ========== ###
 
