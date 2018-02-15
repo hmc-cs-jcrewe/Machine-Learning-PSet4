90ae66f77a3b3e1b958dd2ab5301f889ddc1b6c6
mguills <mattjguil@gmail.com>
2018-02-14 21:52:09 -0800

    finished assignment

--- source/phoneme.py
+++ source/phoneme.py
@@ -72,6 +72,7 @@ def cv_performance_one_trial(clf, train_data, kf) :
 
     ### ========== TODO : START ========== ###
     index = 0
+    # compute different splits of the data
     for train_index, test_index in kf.split(train_data.X, train_data.y):
         X_train, X_test = train_data.X[train_index], train_data.X[test_index]
         y_train, y_test = train_data.y[train_index], train_data.y[test_index]
@@ -82,7 +83,6 @@ def cv_performance_one_trial(clf, train_data, kf) :
     ### ========== TODO : END ========== ###
 
     return scores
-
 
 ######################################################################
 # main
@@ -105,7 +105,7 @@ def main() :
     ### ========== TODO : START ========== ###
 
     # create model_selection.KFold for computing classifier performance
-    kfs = [model_selection.KFold(n_splits=10, shuffle=True) for i in range(10)]
+    kfs = [model_selection.KFold(n_splits=10, shuffle=True, random_state=i) for i in range(10)]
 
     # computes classifier performance for each classifier
     clf1 = Perceptron(max_iter = 1000)
@@ -113,11 +113,13 @@ def main() :
     perceptron_scores = perceptron_scores.flatten()
     print perceptron_scores
 
+    # create dummy classifier to predict the most frequent label
     clf2 = DummyClassifier(strategy = 'most_frequent')
     dummy_scores = cv_performance(clf2, train_data, kfs)
     dummy_scores = dummy_scores.flatten()
     print dummy_scores
 
+    # create logistical regression model with high C value to limit regularization
     clf3 = LogisticRegression(C = 1e5)
     logistic_scores = cv_performance(clf3, train_data, kfs)
     logistic_scores = logistic_scores.flatten()
@@ -132,10 +134,21 @@ def main() :
     logistic_mean = np.mean(logistic_scores, dtype=np.float64)
     logistic_sdev = np.std(logistic_scores, dtype=np.float64)
 
+    t_perc_dum, p_perc_dum = stats.ttest_rel(perceptron_scores, dummy_scores)
+    t_perc_log, p_perc_log = stats.ttest_rel(perceptron_scores, logistic_scores)
+    t_dum_log, p_dum_log = stats.ttest_rel(dummy_scores, logistic_scores)
+
     print 'the perceptron mean is: %03.3f. The standard deviation is: %03.3f' % (perceptron_mean, perceptron_sdev)
     print 'the dummy mean is: %03.3f. The standard deviation is: %03.3f' % (dummy_mean, dummy_sdev)
     print 'the logistic mean is: %03.3f. The standard deviation is: %03.3f' % (logistic_mean, logistic_sdev)
+    print 'the p-value for perceptron vs dummy is: %f' % p_perc_dum
+    print 'the p-value for perceptron vs logistic is: %f' % p_perc_log
+    print 'the p-value for dummy vs logistic is: %f' % p_dum_log
+    print 'the t-statistic for perceptron vs dummy is: %f' % t_perc_dum
+    print 'the t-statistic for perceptron vs logistic is: %f' % t_perc_log
+    print 'the t-statistic for dummy vs logistic is: %f' % t_dum_log
 
+    # scales the data
     X_scaled = preprocessing.scale(train_data.X)
     train_scaled = Data(X_scaled, train_data.y)
 
@@ -164,17 +177,60 @@ def main() :
     logistic_mean_scaled = np.mean(logistic_scores_scaled, dtype=np.float64)
     logistic_sdev_scaled = np.std(logistic_scores_scaled, dtype=np.float64)
 
+    t_perc_dum_s, p_perc_dum_s = stats.ttest_rel(perceptron_scores_scaled, dummy_scores_scaled)
+    t_perc_log_s, p_perc_log_s = stats.ttest_rel(perceptron_scores_scaled, logistic_scores_scaled)
+    t_dum_log_s, p_dum_log_s = stats.ttest_rel(dummy_scores_scaled, logistic_scores_scaled)
+
     print 'the perceptron mean_scaled is: %03.3f. The standard deviation is: %03.3f' % (perceptron_mean_scaled, perceptron_sdev_scaled)
     print 'the dummy mean_scaled is: %03.3f. The standard deviation is: %03.3f' % (dummy_mean_scaled, dummy_sdev_scaled)
     print 'the logistic mean_scaled is: %03.3f. The standard deviation is: %03.3f' % (logistic_mean_scaled, logistic_sdev_scaled)
+    print 'the p-value for perceptron vs dummy is: %f' % p_perc_dum_s
+    print 'the p-value for perceptron vs logistic is: %f' % p_perc_log_s
+    print 'the p-value for dummy vs logistic is: %f' % p_dum_log_s
+    print 'the t-statistic for perceptron vs dummy is: %f' % t_perc_dum_s
+    print 'the t-statistic for perceptron vs logistic is: %f' % t_perc_log_s
+    print 'the t-statistic for dummy vs logistic is: %f' % t_dum_log_s
 
 
-    ### ========== TODO : END ========== ###
+    # create bar chart to visualize results
+    N = 2
+    perceptron_means = (perceptron_mean, perceptron_mean_scaled)
+    perceptron_sdevs = (perceptron_sdev, perceptron_sdev_scaled)
 
-    ### ========== TODO : START ========== ###
-    # part e: plot
+    logistic_means = (logistic_mean, logistic_mean_scaled)
+    logistic_sdevs = (logistic_sdev, logistic_sdev_scaled)
 
-    ### ========== TODO : END ========== ###
+    ind = np.arange(N)
+    width = 0.35
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(ind, perceptron_means, width, color='r', yerr=perceptron_sdevs)
+    rects2 = ax.bar(ind + width, logistic_means, width, color='b', yerr=logistic_sdevs)
+
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Accuracy by Preprocessing and Classifier')
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(('No Preprocessing', 'Standardization'))
+    ax.legend((rects1[0], rects2[0]), ('Perceptron', 'Logistic Regression'))
+    ax.set_ylim([0, 1.4])
+    ax.axhline(y=dummy_mean, color='k')
+    ax.axhline(y=dummy_mean-dummy_sdev, color='k', linestyle='--')
+    ax.axhline(y=dummy_mean+dummy_sdev, color='k', linestyle='--')
+
+    def autolabel(rects):
+        """
+        Attach a text label above each bar displaying its height
+        """
+
+        for rect in rects:
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                    '%03.3f' % height, ha='center', va='bottom')
+
+    autolabel(rects1)
+    autolabel(rects2)
+
+    plt.show()
 
 if __name__ == "__main__" :
     main()
